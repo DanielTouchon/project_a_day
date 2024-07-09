efd5859b17a4ba4b773e774db4c773e16e30bd65
Dtouchon <dtouchon13@gmail.com>
2024-07-09 16:30:50 -0300

    day1

---
+++ band_name_generator.py
@@ -0,0 +1,8 @@
+def band_name_generator():
+    print("Hi, I am Band Name Generator.\nLet's begin!\n")
+    user_city = input("What city are you from?\n")
+    user_pet = input("What pet do you have?\n")
+    print("Awesome!\nYou shall be the " + user_city + " " + user_pet + "s!\n")
+
+while True:
+    band_name_generator()
